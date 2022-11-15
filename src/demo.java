f12ef734cc43196b10c7cf430d09f94a6a8962d6
Mato <1350478903@qq.com>
2022-11-15 22:10:05 +0800

    first

---
+++ src/demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author 闫思潮
+ * @Date 15/11/2022 10:05 PM
+ */
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
